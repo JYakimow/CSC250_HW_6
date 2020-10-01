0d01470f136041564209a8d9ca3fbf0779011102
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2020-10-01 00:06:00 -0500

    Version 1.0
    Homework Complete

--- src/BibleBook.java
+++ src/BibleBook.java
@@ -18,4 +18,12 @@ public class BibleBook
 		System.out.println("Total Number of Chapters: " + this.chapterNum);
 		System.out.println("");
 	}
+	
+	public BibleBook(String colonDelimited)
+	{
+		String[] parts = colonDelimited.split(":");
+		this.name = parts[0];
+		this.chapterNum = Integer.parseInt(parts[1]);
+		this.description = (parts[2]);
+	}
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,12 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Driver 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		BibleBook b1 = new BibleBook("Genesis", "The first book of the bible", 50);
-		BibleBook b2 = new BibleBook("Acts", "The gospel according to St. John", 21);
-		BibleBook b3 = new BibleBook("Genesis", "The gospel according to St. Mathew", 28);
-		b1.display();
-		b2.display();
-		b3.display();
+		//BibleBook b1 = new BibleBook("Genesis", "The first book of the bible", 50);
+		//BibleBook b2 = new BibleBook("Acts", "The gospel according to St. John", 21);
+		//BibleBook b3 = new BibleBook("Mathew", "The gospel according to St. Mathew", 28);
+		//b1.display();
+		//b2.display();
+		//b3.display();
+		
+		//BibleBook[] ar = new BibleBook[4];
+		Scanner input = new Scanner(new File(System.getProperty("user.dir") + "/src/input.dat"));
+		while(input.hasNext())
+		{
+			BibleBook a = new BibleBook(input.nextLine());
+			a.display();
+		}
+		input.close();
 	}
 }
